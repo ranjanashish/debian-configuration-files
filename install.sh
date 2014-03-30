ef87781e161705b790cebfdc923f27fbf7a4563d
Ashish Ranjan <hi.ashish26@gmail.com>
2014-03-30 13:39:27 +0530

    Added support for installing powerline fonts.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 declare -r GH_DEB_CONFIGS=https://github.com/ranjanashish/debian-configuration-files.git
+declare -r GH_POWERLINE_FONTS=https://github.com/Lokaltog/powerline-fonts.git
 declare -r GH_OH_MY_ZSH=https://github.com/robbyrussell/oh-my-zsh.git
 declare -r GH_VUNDLE=https://github.com/gmarik/vundle.git
 
@@ -23,6 +24,13 @@ git clone ${GH_DEB_CONFIGS} ~/git/debian-configuration-files >/dev/null 2>&1 && 
     ln -s ~/.xinitrc ~/.xsession 
     printf 'Done!\n'
 } || printf 'Failed!\n'
+
+# install powerline fonts
+printf '\nInstalling powerline fonts... '
+mkdir -p ~/.fonts
+git clone ${GH_POWERLINE_FONTS} ~/.fonts >/dev/null 2>&1 && {
+    fc-cache -f ~/.fonts && printf 'success.\n' || printf 'font cache update FAILED!\n'
+} || printf 'download FAILED!\n'
 
 # install oh-my-zsh
 printf '\nInstalling oh-my-zsh... '
