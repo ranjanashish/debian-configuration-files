509f1b9568072fa2733dd2182291029948443a46
Ashish Ranjan <hi.ashish26@gmail.com>
2014-07-15 15:40:09 +0000

    Better colors for mem and cpu widget

--- home/ashish/.config/awesome/rc.lua
+++ home/ashish/.config/awesome/rc.lua
@@ -121,18 +121,19 @@ mytextclock = awful.widget.textclock()
 -- Create a memory widget
 memwidget = awful.widget.progressbar()
 memwidget:set_width(50)
-memwidget:set_height(10)
 memwidget:set_vertical(true)
 memwidget:set_background_color("#494B4F")
 memwidget:set_border_color(nil)
-memwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, {1, "#AECF96"}}})
+memwidget:set_color("#00FFFF")
+vicious.cache(vicious.widgets.mem)
 vicious.register(memwidget, vicious.widgets.mem, "$1", 13)
 
 -- Create a cpu widget
 cpuwidget = awful.widget.graph()
 cpuwidget:set_width(50)
 cpuwidget:set_background_color("#494B4F")
-cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, {1, "#AECF96" }}})
+cpuwidget:set_color("#7FFF00")
+vicious.cache(vicious.widgets.cpu)
 vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
 
 -- Create a wibox for each screen and add it
